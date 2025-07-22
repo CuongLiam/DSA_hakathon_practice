8f731e6cb08fcb1bccfd0fcfa69c34c274f52ed9
Cuong Liam <cuongdeptrai2706@gmail.com>
2025-07-22 10:30:19 +0700

    update

--- main.c
+++ main.c
@@ -117,6 +117,46 @@ Node* deleteStudent(Node* head) {
     return head;
 }
 
+void updateValue(Node* node) {
+    printf("nhap ten: ");
+    fgets(node->data.name, MAX, stdin);
+    fflush(stdin);
+
+    printf("grade: ");
+    scanf("%d", &node->data.grade);
+    fflush(stdin);
+
+    printf("note: ");
+    fgets(node->data.note, MAX, stdin);
+}
+
+Node* updateStudent(Node* head) {
+    if (head == NULL) {
+        printf("ds rong\n");
+        return NULL;
+    }
+
+    int idToUpdate;
+    printf("\nnhap id muon cap nhat");
+    scanf("%d", &idToUpdate);
+    fflush(stdin);
+
+    if (head->data.id == idToUpdate) {
+        updateValue(head);
+        return head;
+    }
+
+    Node* curr = head;
+    while (curr != NULL) { // ptr point to a temp ptr to update muahhahaha
+        if (idToUpdate == curr->next->data.id) {
+            updateValue(curr);
+        }
+        curr = curr->next;
+    }
+    printf("id ko ton tai");
+    return head;
+}
+
 int main(void) {
     Node* stuHead = NULL;
 
@@ -137,13 +177,16 @@ int main(void) {
             }
             case 3: {
                 stuHead = deleteStudent(stuHead);
-                if (stuHead == NULL) {
-                    printf("da xoa\n");
-                } else
                 printf("da xoa\n");
                 break;
             }
-
+            case 4: {
+                stuHead = updateStudent(stuHead);
+                if (stuHead != NULL) {
+                    printf("da cap nhat");
+                }
+                break;
+            }
             default:
                 break;
         }
